f793e205498159c58ce1694d7a9c43aa4e4291a3
vtu30121-ship-it <vtu30121@veltech.edu.in>
2026-09-01 21:32:23 +0530

    Create date and time.java

---
+++ date and time.java
@@ -0,0 +1,13 @@
+import java.util.Calendar;
+
+class Result {
+    public static String findDay(int month, int day, int year) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month - 1, day);
+        int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+        
+        String[] days = {"SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"};
+        return days[dayOfWeek - 1];
+    }
+
+    }
